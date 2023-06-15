da26e9b17dce80fdd1c2c430136e1378be51fbdf
AnshulBompada <ancient.falme1145@gmail.com>
2023-06-15 03:16:52 -0500

    Got rid of warnings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,6 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Libs.SProfile;
-import frc.robot.Libs.Telemetry;
 
 public class RobotContainer {
   public RobotContainer() {
