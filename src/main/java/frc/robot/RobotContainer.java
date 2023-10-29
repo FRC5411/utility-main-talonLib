c125ac7c9a6279cc66984771f03cbc84c5c161b7
AnshulBompada <ancient.falme1145@gmail.com>
2023-10-28 19:40:47 -0500

    Added linear reg

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,11 +1,9 @@
 package frc.robot;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
   public RobotContainer() {
-    XboxController controller = new XboxController(0);
 
     configureBindings();
   }
